d3753fda0821aa8f6219e6ef69d43d2aba94b4c9
ily000ttt <146103468+ily000ttt@users.noreply.github.com>
2024-02-29 18:32:49 +0300

    Add files via upload

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -11,27 +11,35 @@ const Keyboard ={
         onclose: null
     },
 
-    propertise:{
+    properties:{
         value: "",
         capsLock: false
     },
 
     
     init() {
-        // Create main elements
+        
         this.elements.main = document.createElement("div");
         this.elements.keysContainer = document.createElement("div");
 
-        // Setup main elements
-        this.elements.main.classList.add("keyboard", "1keyboard--hidden");
+        
+        this.elements.main.classList.add("keyboard", "keyboard--hidden");
         this.elements.keysContainer.classList.add("keyboard__keys");
         this.elements.keysContainer.appendChild(this._createKeys());
 
         this.elements.keys = this.elements.keysContainer.querySelectorAll(".keyboard__key");
 
-        // Add to DOM
+        
         this.elements.main.appendChild(this.elements.keysContainer);
         document.body.appendChild(this.elements.main);
+
+        document.querySelectorAll(".use-keyboard-input").forEach(element =>{
+            element.addEventListener("focus", () =>{
+                this.open(element.value, currentValue =>{
+                    element.value = currentValue;
+                })
+            })
+        })
     },
     _createKeys(){
         const fragment = document.createDocumentFragment();
@@ -118,6 +126,7 @@ const Keyboard ={
                     keyElement.addEventListener("click", () => {
                         this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
                         this._triggerEvent("oninput");
+
                     });
 
                     break;
@@ -134,17 +143,32 @@ const Keyboard ={
     },
     _triggerEvent(handlerName)
     {
-        console.log(handlerName)
+        if(typeof this.eventHandlers[handlerName]=="function"){
+            this.eventHandlers[handlerName](this.properties.value);
+        }
     },
     _toggleCapsLock(){
-        console.log("Caps");
+        this.properties.capsLock = !this.properties.capsLock
+        for (const key of this.elements.keys)
+        {
+            if(key.childElementCount === 0)
+            {
+                key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+            }
+        }
     },
     open(initialValue, oninput, onclose){
-
+        this.properties.value = initialValue || "";
+        this.eventHandlers.oninput =oninput
+        this.eventHandlers.onclose = onclose;
+        this.elements.main.classList.remove("keyboard--hidden")
     },
 
     close(){
-
+        this.properties.value = "";
+        this.eventHandlers.oninput = oninput;
+        this.eventHandlers.onclose = onclose;
+        this.elements.main.classList.add("keyboard--hidden")
     }
 };
 
